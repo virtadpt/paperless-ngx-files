0cd48db29352da3434967fc66e1b273456cf80be
The Doctor <drwho@virtadpt.net>
2023-03-03 18:07:12 -0800

    Initial commits of my paperless-ngx shell scripts and template config file.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ paperless-consumer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export VIRTUAL_ENV="$(pwd)/env"
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export PS1="(virtualenv) $PS1"
+unset PYTHON_HOME
+
+cd src/
+eval exec "python ./manage.py document_consumer"
+
+exit 0
---
+++ paperless-webserver.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export VIRTUAL_ENV="$(pwd)/env"
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export PS1="(virtualenv) $PS1"
+unset PYTHON_HOME
+
+cd src/
+eval exec "gunicorn -c gunicorn.conf.py paperless.asgi:application"
+
+exit 0
